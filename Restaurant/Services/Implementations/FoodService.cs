61b2eb13aadf6c039ef6cf456f57fbc78fa70d80
Kikemimi1 <abosedekikelomo408@gmail.com>
2026-09-04 01:27:26 -0400

    Implement CreateFoodAsync service logic

--- Restaurant/Services/Implementations/FoodService.cs
+++ Restaurant/Services/Implementations/FoodService.cs
@@ -1,14 +1,30 @@
-﻿using Restaurant.DTOs;
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.Data;
+using Restaurant.DTOs;
 using Restaurant.DTOs.ResponseDTOs;
+using Restaurant.Entities;
 using Restaurant.Services.Interfaces;
 
 public class FoodService : IFoodService
 {
-    public Task<CreateFoodResponse> CreateFoodAsync(
-        CreateFoodRequest request,
-        Guid userId)
+    public Task<CreateFoodResponse> CreateFoodAsync(CreateFoodRequest request, Guid userId)
     {
-        throw new NotImplementedException();
+
+        var food = new Food
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            Type = request.Type,
+            Price = request.Price,
+            Description = request.Description,
+            CreatedAt = DateTime.UtcNow,
+            CreatedByUserId = userId,
+            IsDeleted = false   
+        };
+
+        _context.Foods.Add(food);
+
+
     }
 
     public Task<IEnumerable<CreateFoodResponse>> GetAllFoodAsync()
@@ -16,10 +32,7 @@ public class FoodService : IFoodService
         throw new NotImplementedException();
     }
 
-    public Task<UpdateFoodResponse> UpdateFoodAsync(
-        Guid foodId,
-        UpdateFoodRequest request,
-        Guid userId)
+    public Task<UpdateFoodResponse> UpdateFoodAsync( Guid foodId, UpdateFoodRequest request,Guid userId)
     {
         throw new NotImplementedException();
     }
