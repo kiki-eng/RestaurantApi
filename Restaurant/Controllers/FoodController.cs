cfd060aaa814a5571a2170bc2ad66092d3c9c584
Kikemimi1 <abosedekikelomo408@gmail.com>
2026-09-06 05:44:25 -0400

    Complete JWT auth and food CRUD flow

--- Restaurant/Controllers/FoodController.cs
+++ Restaurant/Controllers/FoodController.cs
@@ -1,18 +1,76 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Restaurant.DTOs;
+using Restaurant.Services;
 using Restaurant.Services.Interfaces;
+using System.Security.Claims;
 
 namespace Restaurant.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
     public class FoodController : ControllerBase
     {
-        private readonly IFoodService _foodservice;
+        private readonly IFoodService _foodService;
+
         public FoodController(IFoodService foodService)
         {
-            _foodservice = foodService;
+            _foodService = foodService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFood(CreateFoodRequest request)
+        {
+            var userId = Guid.Parse(
+                User.FindFirstValue(ClaimTypes.NameIdentifier)
+            );
+
+            var response = await _foodService.CreateFoodAsync(request, userId);
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllFood()
+        {
+            var userId = Guid.Parse(
+                User.FindFirstValue(ClaimTypes.NameIdentifier)
+            );
+
+            var response = await _foodService.GetAllFoodAsync(userId);
+
+            return Ok(response);
+        }
+
+        [HttpPut("{foodId}")]
+        public async Task<IActionResult> UpdateFood(
+            Guid foodId,
+            UpdateFoodRequest request)
+        {
+            var userId = Guid.Parse(
+                User.FindFirstValue(ClaimTypes.NameIdentifier)
+            );
+
+            var response = await _foodService.UpdateFoodAsync(
+                foodId,
+                request,
+                userId
+            );
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{foodId}")]
+        public async Task<IActionResult> DeleteFood(Guid foodId)
+        {
+            var userId = Guid.Parse(
+                User.FindFirstValue(ClaimTypes.NameIdentifier)
+            );
+
+            await _foodService.DeleteFoodAsync(foodId, userId);
+
+            return NoContent();
         }
     }
-
 }
--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -2,6 +2,8 @@ using FluentValidation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi;
 using Restaurant.Data;
 using Restaurant.Services.Implementations;
 using Restaurant.Services.Interfaces;
@@ -9,19 +11,21 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Controllers
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
-//builder.Services.AddOpenApi();
-
+// Database
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
+// FluentValidation
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
-var jwtKey = builder.Configuration["Jwt:Key"];
+// JWT
+var jwtKey = builder.Configuration["Jwt:Key"]
+    ?? throw new InvalidOperationException("JWT key is missing.");
 
 builder.Services
     .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -38,40 +42,52 @@ builder.Services
             ValidAudience = builder.Configuration["Jwt:Audience"],
 
             IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(jwtKey!))
+                Encoding.UTF8.GetBytes(jwtKey)
+            )
         };
     });
 
 builder.Services.AddAuthorization();
+
+// Dependency Injection
 builder.Services.AddScoped<IJwtService, JwtService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IFoodService, FoodService>();
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter your JWT token"
+    });
 
-
-
-
+    options.AddSecurityRequirement(document =>
+        new OpenApiSecurityRequirement
+        {
+            [new OpenApiSecuritySchemeReference("Bearer", document)] = []
+        });
+});
 
 var app = builder.Build();
 
+// Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-   // app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
-
 app.UseAuthorization();
 
 app.MapControllers();
