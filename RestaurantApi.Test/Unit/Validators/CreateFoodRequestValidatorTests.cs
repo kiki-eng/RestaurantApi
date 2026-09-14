75eaa091472c8439876848b928d9f2115966f66e
Kikemimi1 <abosedekikelomo408@gmail.com>
2026-09-14 18:34:10 -0400

    test: add valid food description validation test

--- RestaurantApi.Test/Unit/Validators/CreateFoodRequestValidatorTests.cs
+++ RestaurantApi.Test/Unit/Validators/CreateFoodRequestValidatorTests.cs
@@ -193,5 +193,21 @@ namespace RestaurantApi.Test.Unit.Validators
 
             result.ShouldHaveValidationErrorFor(x => x.Description);
         }
+
+        [Fact]
+        public void Description_WhenValid_ShouldNotHaveValidationError()
+        {
+            var model = new CreateFoodRequest
+            {
+                Name = "Test Food",
+                Type = "Main Course",
+                Price = 10.00m,
+                Description = "Valid food description"
+            };
+
+            var result = _validators.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.Description);
+        }
     }
 }
