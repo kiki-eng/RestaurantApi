64a93329be8a28ecffd63d06f33f9a6c2146b2db
Kikemimi1 <abosedekikelomo408@gmail.com>
2026-09-14 18:28:10 -0400

    test: add valid food type validation test

--- RestaurantApi.Test/Unit/Validators/CreateFoodRequestValidatorTests.cs
+++ RestaurantApi.Test/Unit/Validators/CreateFoodRequestValidatorTests.cs
@@ -130,5 +130,20 @@ namespace RestaurantApi.Test.Unit.Validators
 
             result.ShouldHaveValidationErrorFor(x => x.Type);
         }
+
+        [Fact]
+        public void Type_WhenValid_ShouldNotHaveValidationError()
+        {
+            var model = new CreateFoodRequest
+            {
+                Name = "Test Food",
+                Type = "Main Course",
+                Price = 10.00m,
+                Description = "Valid food description"
+            };
+
+            var result = _validators.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(x => x.Type);
+        }
     }
 }
