3a5405b3af1991a041a2dfee0afd4984dbd305e3
Kikemimi1 <abosedekikelomo408@gmail.com>
2026-09-14 16:51:06 -0400

    test: add CreateFoodRequest validator unit tests

---
+++ RestaurantApi.Test/Unit/Validators/CreateFoodRequestValidatorTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantApi.Test.Unit.Validators
+{
+    internal class CreateFoodRequestValidatorTests
+    {
+    }
+}
