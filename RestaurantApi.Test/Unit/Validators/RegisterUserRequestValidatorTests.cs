6e88b29b297fe1f235c2414ecfd9a351ef670ed5
Kikemimi1 <abosedekikelomo408@gmail.com>
2026-09-14 19:46:21 -0400

    test: add valid registration name validation test

---
+++ RestaurantApi.Test/Unit/Validators/RegisterUserRequestValidatorTests.cs
@@ -0,0 +1,35 @@
+﻿using Restaurant.DTOs;
+using Restaurant.Validators;
+using FluentValidation.TestHelper;
+
+
+namespace RestaurantApi.Test.Unit.Validators
+{
+    public class RegisterUserRequestValidatorTests
+    {
+
+        private readonly RegisterUserRequestValidator _request;
+
+        public RegisterUserRequestValidatorTests()
+        {
+            _request = new RegisterUserRequestValidator();
+        }
+
+        [Fact]
+        public void Name_WhenValid_ShouldNotHaveValidationError()
+        {
+            var model = new RegisterUserRequest
+            {
+                Name = "Test User",
+                Email = "test@example.com",
+                Password = "Password123",
+                Location = "Toronto"
+            };
+
+            var result = _request.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
+        }
+
+    }
+}
