fcb4f9390d1da09d5609d570d01f15bbf2f2db4d
Kikemimi1 <abosedekikelomo408@gmail.com>
2026-09-14 16:57:51 -0400

    test: add zero price validation test

--- RestaurantApi.Test/Unit/Validators/CreateFoodRequestValidatorTests.cs
+++ RestaurantApi.Test/Unit/Validators/CreateFoodRequestValidatorTests.cs
@@ -1,10 +1,56 @@
-﻿using System;
+﻿using Restaurant.DTOs;
+using Restaurant.Validators;
+using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Xml.Linq;
 
 namespace RestaurantApi.Test.Unit.Validators
 {
-    internal class CreateFoodRequestValidatorTests
+    public class CreateFoodRequestValidatorTests
     {
+        private readonly CreateFoodRequestValidator _validators;
+        public CreateFoodRequestValidatorTests()
+        { 
+            _validators = new CreateFoodRequestValidator();
+
+        }
+
+        [Fact]
+        public void Name_WhenEmpty_ShouldHaveValidationError()
+        {
+            var model = new CreateFoodRequest
+            {
+                Name = "",
+                Type = "Main",
+                Price = 10,
+                Description = "Test food"
+            };
+
+        }
+        [Fact]
+        public void Name_WhenValid_ShouldNotHaveValidationError()
+        {
+            var model = new CreateFoodRequest
+            {
+                Name = "Jollof Rice",
+                Type = "Main",
+                Price = 10,
+                Description = "Test food"
+            };
+        }
+
+        public void Price_WhenZero_ShouldHaveValidationError()
+        {
+            var model = new CreateFoodRequest
+            {
+                Name = "Jollof Rice",
+                Type = "Main",
+                Price = 0,
+                Description = "Test food"
+
+            };
+        }
     }
 }
