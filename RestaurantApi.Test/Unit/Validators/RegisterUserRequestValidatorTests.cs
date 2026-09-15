a1c6bb3910a952b3285f5923725cdc9ca49a4830
Kikemimi1 <abosedekikelomo408@gmail.com>
2026-09-14 20:29:57 -0400

    refactor: reduce duplication in registration validator tests

--- RestaurantApi.Test/Unit/Validators/RegisterUserRequestValidatorTests.cs
+++ RestaurantApi.Test/Unit/Validators/RegisterUserRequestValidatorTests.cs
@@ -1,13 +1,11 @@
-﻿using Restaurant.DTOs;
+﻿using FluentValidation.TestHelper;
+using Restaurant.DTOs;
 using Restaurant.Validators;
-using FluentValidation.TestHelper;
-
 
 namespace RestaurantApi.Test.Unit.Validators
 {
     public class RegisterUserRequestValidatorTests
     {
-
         private readonly RegisterUserRequestValidator _request;
 
         public RegisterUserRequestValidatorTests()
@@ -15,16 +13,21 @@ namespace RestaurantApi.Test.Unit.Validators
             _request = new RegisterUserRequestValidator();
         }
 
-        [Fact]
-        public void Name_WhenValid_ShouldNotHaveValidationError()
+        private static RegisterUserRequest CreateValidRequest()
         {
-            var model = new RegisterUserRequest
+            return new RegisterUserRequest
             {
                 Name = "Test User",
                 Email = "test@example.com",
                 Password = "Password123",
                 Location = "Toronto"
             };
+        }
+
+        [Fact]
+        public void Name_WhenValid_ShouldNotHaveValidationError()
+        {
+            var model = CreateValidRequest();
 
             var result = _request.TestValidate(model);
 
@@ -34,13 +37,8 @@ namespace RestaurantApi.Test.Unit.Validators
         [Fact]
         public void Name_WhenEmpty_ShouldHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = "",
-                Email = "test@example.com",
-                Password = "Password123",
-                Location = "Toronto"
-            };
+            var model = CreateValidRequest();
+            model.Name = "";
 
             var result = _request.TestValidate(model);
 
@@ -50,13 +48,8 @@ namespace RestaurantApi.Test.Unit.Validators
         [Fact]
         public void Name_WhenMoreThan100Characters_ShouldHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = new string('A', 101),
-                Email = "test@example.com",
-                Password = "Password123",
-                Location = "Toronto"
-            };
+            var model = CreateValidRequest();
+            model.Name = new string('A', 101);
 
             var result = _request.TestValidate(model);
 
@@ -66,13 +59,8 @@ namespace RestaurantApi.Test.Unit.Validators
         [Fact]
         public void Name_WhenExactly100Characters_ShouldNotHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = new string('A', 100),
-                Email = "test@example.com",
-                Password = "Password123",
-                Location = "Toronto"
-            };
+            var model = CreateValidRequest();
+            model.Name = new string('A', 100);
 
             var result = _request.TestValidate(model);
 
@@ -82,13 +70,8 @@ namespace RestaurantApi.Test.Unit.Validators
         [Fact]
         public void Email_WhenEmpty_ShouldHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = "Test User",
-                Email = "",
-                Password = "Password123",
-                Location = "Toronto"
-            };
+            var model = CreateValidRequest();
+            model.Email = "";
 
             var result = _request.TestValidate(model);
 
@@ -98,43 +81,29 @@ namespace RestaurantApi.Test.Unit.Validators
         [Fact]
         public void Email_WhenInvalid_ShouldHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = "Test User",
-                Email = "invalid-email",
-                Password = "Password123",
-                Location = "Toronto"
-            };
+            var model = CreateValidRequest();
+            model.Email = "invalid-email";
 
             var result = _request.TestValidate(model);
 
             result.ShouldHaveValidationErrorFor(x => x.Email);
         }
+
         [Fact]
         public void Email_WhenValid_ShouldNotHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = "Test User",
-                Email = "test@example.com",
-                Password = "Password123",
-                Location = "Toronto"
-            };
+            var model = CreateValidRequest();
 
             var result = _request.TestValidate(model);
 
             result.ShouldNotHaveValidationErrorFor(x => x.Email);
         }
+
         [Fact]
         public void Password_WhenEmpty_ShouldHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = "Test User",
-                Email = "test@example.com",
-                Password = "",
-                Location = "Toronto"
-            };
+            var model = CreateValidRequest();
+            model.Password = "";
 
             var result = _request.TestValidate(model);
 
@@ -144,13 +113,8 @@ namespace RestaurantApi.Test.Unit.Validators
         [Fact]
         public void Password_WhenLessThan8Characters_ShouldHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = "Test User",
-                Email = "test@example.com",
-                Password = "Pass123",
-                Location = "Toronto"
-            };
+            var model = CreateValidRequest();
+            model.Password = "Pass123";
 
             var result = _request.TestValidate(model);
 
@@ -160,28 +124,19 @@ namespace RestaurantApi.Test.Unit.Validators
         [Fact]
         public void Password_WhenExactly8Characters_ShouldNotHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = "Test User",
-                Email = "test@example.com",
-                Password = "Pass1234",
-                Location = "Toronto"
-            };
+            var model = CreateValidRequest();
+            model.Password = "Pass1234";
 
             var result = _request.TestValidate(model);
 
             result.ShouldNotHaveValidationErrorFor(x => x.Password);
         }
+
         [Fact]
         public void Location_WhenEmpty_ShouldHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = "Test User",
-                Email = "test@example.com",
-                Password = "Password123",
-                Location = ""
-            };
+            var model = CreateValidRequest();
+            model.Location = "";
 
             var result = _request.TestValidate(model);
 
@@ -191,13 +146,7 @@ namespace RestaurantApi.Test.Unit.Validators
         [Fact]
         public void Location_WhenValid_ShouldNotHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = "Test User",
-                Email = "test@example.com",
-                Password = "Password123",
-                Location = "Toronto"
-            };
+            var model = CreateValidRequest();
 
             var result = _request.TestValidate(model);
 
@@ -207,13 +156,8 @@ namespace RestaurantApi.Test.Unit.Validators
         [Fact]
         public void Location_WhenMoreThan100Characters_ShouldHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = "Test User",
-                Email = "test@example.com",
-                Password = "Password123",
-                Location = new string('A', 101)
-            };
+            var model = CreateValidRequest();
+            model.Location = new string('A', 101);
 
             var result = _request.TestValidate(model);
 
@@ -223,18 +167,12 @@ namespace RestaurantApi.Test.Unit.Validators
         [Fact]
         public void Location_WhenExactly100Characters_ShouldNotHaveValidationError()
         {
-            var model = new RegisterUserRequest
-            {
-                Name = "Test User",
-                Email = "test@example.com",
-                Password = "Password123",
-                Location = new string('A', 100)
-            };
+            var model = CreateValidRequest();
+            model.Location = new string('A', 100);
 
             var result = _request.TestValidate(model);
 
             result.ShouldNotHaveValidationErrorFor(x => x.Location);
         }
-
     }
 }
