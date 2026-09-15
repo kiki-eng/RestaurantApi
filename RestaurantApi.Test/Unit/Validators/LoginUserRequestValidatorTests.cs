05fbacb9ff056d14e7d3610654adb69697d531d6
Kikemimi1 <abosedekikelomo408@gmail.com>
2026-09-14 23:23:40 -0400

    test: add empty login email validation test

---
+++ RestaurantApi.Test/Unit/Validators/LoginUserRequestValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.TestHelper;
+using Restaurant.DTOs;
+using Restaurant.Validators;
+
+namespace RestaurantApi.Test.Unit.Validators
+{
+    public class LoginUserRequestValidatorTests
+    {
+        private readonly LoginUserRequestValidator _validator;
+
+        public LoginUserRequestValidatorTests()
+        {
+            _validator = new LoginUserRequestValidator();
+        }
+
+
+        private static LoginUserRequest CreateValidRequest()
+        {
+            return new LoginUserRequest
+            {
+                Email = "test@example.com",
+                Password = "Password123"
+            };
+        }
+
+        [Fact]
+        public void Email_WhenEmpty_ShouldHaveValidationError()
+        {
+            var model = CreateValidRequest();
+            model.Email = "";
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+    }
+}
